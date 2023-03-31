e3c95685adeaaed3cd75809913c8e3c88c554f0c
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-04-01 01:14:24 +0530

    code formatting

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import express from 'express';
+import userRoutes from './src/routes/user.js';
 dotenv.config();
 
-export const connectToDatabase = async ()=>{
-
-    if(mongoose.connections[0]){
-        return;
-    }
-    await mongoose.connect(process.env.CONNECTION_URL).
+const app = express();
+mongoose.connect(process.env.CONNECTION_URL).
     then(()=>console.log("Connected"))
-    .catch((err)=>console.log(err))
-}
+    .catch((err)=>console.log("Database not connected"))
+
+const PORT = process.env.PORT || 3300;
+app.listen(PORT, ()=> {
+    console.log(`Minigram server is running on port ${PORT}`);
+})
+
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
+
+app.use('/user',userRoutes);
