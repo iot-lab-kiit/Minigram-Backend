290d0f20bbbd8fb4428f49866da68894435a3032
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-04-01 16:04:49 +0530

    code formatting

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ import userRoutes from './src/routes/user.js';
 dotenv.config();
 
 const app = express();
-mongoose.connect(process.env.CONNECTION_URL).
-    then(()=>console.log("Connected"))
-    .catch((err)=>console.log("Database not connected"))
 
 const PORT = process.env.PORT || 3300;
-app.listen(PORT, ()=> {
-    console.log(`Minigram server is running on port ${PORT}`);
-})
+mongoose.connect(process.env.CONNECTION_URL)
+    .then(()=>app.listen(PORT, ()=> {
+        console.log(`Minigram server is running on port ${PORT}`);
+    }))
+    .catch((err)=>console.log(err))
+
 
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
